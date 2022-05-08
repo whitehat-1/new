b67f97ce18622351aa24d8d4ed061ac97ed7f8bd
M J <69307802+whitehat-1@users.noreply.github.com>
2022-05-08 22:16:31 +0100

    new changes

--- new2/models.py
+++ new2/models.py
@@ -4,7 +4,7 @@ from tortoise.models import Model
 
 class Tasklist(Model):
     task_id = fields.UUIDField(pk=True)
-    property= fields.CharField(max_length=400)
+    Property = fields.CharField(max_length=400)
     description = fields.CharField(max_length=400)
     date_created_at = fields.DatetimeField(auto_now_add=True)
     complete_task = fields.BooleanField(default=False)
