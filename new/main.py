296926e877c3e32f8785739cfc066e533fd4e9e4
M J <69307802+whitehat-1@users.noreply.github.com>
2022-05-09 03:41:57 -0700

    Update main.py

--- new/main.py
+++ new/main.py
@@ -1,6 +1,6 @@
 from asyncio import Task
 from typing import List
-from fastapi import FastAPI
+from fastapi import FastAPI ,HTTPException
 
 from database import create_start_app_handler
 from models import Task
@@ -25,6 +25,18 @@ async def home(data: TaskCreate):
         **data.dict(exclude_unset=True) #convert a pydantic model to a dictionary
     )
     return Task
+
+@app.put('/task_get/{id}', response_model=Taskpublic)
+async def update_task(id: str):
+    get_task = await Task.get(id=id)
+    if not get_task:
+        return  HTTPException(status_code=404, detail="User ID Not Found!!")
+    get_task.task_completed=True
+    await get_task.save()
+    return await get_task
+
+
+
 
 @app.get("/tasks", response_model=List[Taskpublic])
 async def home(data: TaskCreate):
